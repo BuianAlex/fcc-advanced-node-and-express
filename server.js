ca27f3f85bd46170350f3a5b16eef4ab50fd5ac6
BuianAlex <alex_bu@Dell.localdomain>
2020-06-27 18:39:33 +0300

    1 step

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const express = require('express');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.route("/").get((req, res) => {
+app.set('view engine', 'pug');
+app.route('/').get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render(__dirname + '/views/pug/index.pug');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  console.log('Listening on port ' + process.env.PORT);
 });
